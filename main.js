d36afcacd6092a410974c18eb620cd3e094d34c4
siva <sivashakthiarun.c@gmail.com>
2022-09-13 10:23:38 +0530

    repush

---
+++ main.js
@@ -0,0 +1,23 @@
+/*var fabevy=["Don","Shakthi","Ajay","Wikki"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+var a=fabevy.indexOf("Shakthi");
+var b=fabevy.indexOf("RS")
+console.log("Method:fabevy.indexOf();");
+console.log("Change in array:",a);
+console.log("Change in array:",b);
+console.log("Return:Length of the array after indexOf(); is"+a.length);
+console.log("Return:Length of the array after indexOf(); is"+b.length);
+*/
+
+var fabevy=["Don","Shakthi","Ajay","Wikki"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+var a=fabevy.indexOf("Ajay",3);
+var b=fabevy.indexOf("Shakthi",0)
+console.log("Method:fabevy.indexOf();");
+console.log("Change in array:",a);
+console.log("Change in array:",b);
+console.log("Return:Length of the array after indexOf(); is"+a.length);
+console.log("Return:Length of the array after indexOf(); is"+b.length);
+
